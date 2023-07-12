74c98a6af65844341cb01a09dfe7dd91e2418257
raphaelfurtado.f@gmail.com <raphaelfurtado.f@gmail.com>
2023-07-12 19:10:03 +0000

    Create the employees, add a line in the end of the sources file and change the project materialized and schemas

---
+++ models/marts/dim_employees.sql
@@ -0,0 +1,35 @@
+with 
+    employees as (
+        select
+        *
+        from {{ ref('stg_erp__employees') }}
+    )
+ 
+    , managements as (
+        select
+        *
+        from {{ ref('stg_erp__employees') }}
+    )
+
+    , join_table as (
+        select 
+        employees.employee_id
+        , employees.first_name
+        , employees.last_name
+        , employees.full_name
+        , managements.full_name as management_full_name
+        , employees.birth_date
+        , employees.hire_date
+        , employees.address
+        , employees.city
+        , employees.region
+        , employees.postal_code
+        , employees.country
+        , employees.notes
+        from employees
+        left join managements 
+        on employees.management_id = managements.employee_id
+    )
+
+select *
+from join_table
