fa6c1ba3cee0ee94ad5f686401b181b6bebb9a81
raphaelfurtado.f@gmail.com <raphaelfurtado.f@gmail.com>
2023-07-14 14:29:57 +0000

    Metrics created

--- models/marts/fct_sales.sql
+++ models/marts/fct_sales.sql
@@ -32,6 +32,7 @@ with
     , join_fact as (
         select 
             sell_orders.surrogate_key
+            , sell_orders.order_id
             , employees.employee_id
             , customers.customer_id
             , products.product_id
@@ -49,6 +50,12 @@ with
             , sell_orders.ship_region
             , sell_orders.ship_postal_code
             , sell_orders.ship_country
+            , employees.employee_full_name
+            , employees.management_full_name
+            , products.product_name
+            , products.category_name
+            , products.company_name
+            , products.is_discontinued
 
         from sell_orders
         left join employees using(employee_id)
@@ -57,5 +64,58 @@ with
         left join shippers using(ship_id)
     )
 
+    , metrics as (
+        select
+        *
+        , round((freight/(count(*) over(partition by order_id))),2) as freight_per_product 
+        , (unit_price * quantity) as gross_income
+        , (unit_price * quantity * (1-discount)) as net_income
+        , (unit_price * quantity * discount) as total_discount
+        , (unit_price + round((freight/(count(*) over(partition by order_id))/quantity),2)) as unit_price_with_freight
+        , date_diff(shipped_date, order_date, day) as time_to_ship
+        , date_diff(required_date, shipped_date, day) as ship_to_required
+        from join_fact
+    )
+
+    , final_select as (
+        select
+            surrogate_key
+            , order_id
+            , employee_id
+            , customer_id
+            , product_id
+            , ship_id
+
+            , unit_price
+            , unit_price_with_freight
+            , quantity
+            , gross_income
+            , discount
+            , net_income
+            , total_discount
+            , freight_per_product
+            , time_to_ship
+            , ship_to_required
+
+            , order_date
+            , required_date
+            , shipped_date   
+
+            , ship_name
+            , ship_address
+            , ship_city
+            , ship_region
+            , ship_postal_code
+            , ship_country
+            , employee_full_name
+            , management_full_name
+            , product_name
+            , category_name
+            , company_name
+            , is_discontinued
+        from metrics
+    )
+
 select *
-from join_fact
+from final_select
+order by ship_to_required
